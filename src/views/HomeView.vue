def3f1875e391f1fef0f142d1b6999781b190f74
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2024-10-25 21:59:01 +0200

    install primevue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>home</main>
+  <main>
+    <Button label="Primary" :loading="true" />
+    <Button label="Secondary" severity="secondary" />
+    <Button label="Success" severity="success" />
+    <Button label="Info" severity="info" />
+    <Button label="Warn" severity="warn" />
+    <Button label="Help" severity="help" />
+    <Button label="Danger" severity="danger" />
+    <Button label="Contrast" severity="contrast" />
+  </main>
 </template>
