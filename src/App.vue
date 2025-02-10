afec8bae9ded3f9c537aa7c5cc99aeb7bf31578e
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2025-02-11 00:04:01 +0100

    ToggleDarkMode.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import ToggleDarkMode from '@/components/ToggleDarkMode.vue'
 </script>
 
 <template>
   <header>
+    <ToggleDarkMode />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
